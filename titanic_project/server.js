824a959577500aa0d6dd7f75cf22476f1424fe20
iloveitwhentheyrunnin <74145318+iloveitwhentheyrunnin@users.noreply.github.com>
2023-10-25 15:33:31 +0200

    clean server.js + env conf with mongo db

--- titanic_project/server.js
+++ titanic_project/server.js
@@ -3,28 +3,55 @@ const path = require('path');
 const bodyparser = require("body-parser");
 const session = require('express-session');
 const { v4: uuidv4 } = require("uuid");
-
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const flash = require("express-flash");
+const MongoStore = require("connect-mongo");
 const router = require("./router")
+
+// ==========
+// App initialization
+// ==========
+
+dotenv.config();
+const {
+  APP_HOSTNAME,
+  APP_PORT,
+  NODE_ENV,
+  MONGO_STRING,
+  MONGO_DB_NAME
+} = process.env;
 
 const app = express();
 
-const port = process.env.PORT||3000;
-
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({extended:true}))
-
 app.set('view engine','twig');
-
-//load assets
-app.use('/static',express.static(path.join(__dirname,"public")))
-app.use('/assets',express.static(path.join(__dirname,"public/assets")))
+app.locals.pretty = NODE_ENV !== 'production';
 
 app.use(session({
     //unique session id with uuid
     secret: uuidv4(),
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: `${MONGO_STRING}${MONGO_DB_NAME}` })
 }))
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
+
+app.use(flash());
+
+// ==========
+// App middlewares
+// ==========
+
+//load assets
+app.use('/static',express.static(path.join(__dirname,"public")))
+app.use('/assets',express.static(path.join(__dirname,"public/assets")))
+
+
+// ==========
+// App routers
+// ==========
 
 app.use('/', router); 
 
@@ -34,4 +61,21 @@ app.get('/',(req,res) =>{
     res.render('index.twig',{title: "The Titanic Tool"});
 })
 
-app.listen(port,()=>{console.log("Listening to the server on http://localhost:3000")})
+try {
+    mongoose.connect(`${MONGO_STRING}${MONGO_DB_NAME}`)
+    console.log('✅ Connecté à la base MongoDB');
+
+    // ==========
+    // App start
+    // ==========
+
+    app.listen(APP_PORT, () => {
+        console.log(`App listening at http://${APP_HOSTNAME}:${APP_PORT}`);
+    });
+  }
+  catch (err) {
+    console.error('Erreur de connexion', err.message)
+}
+
+
+  
