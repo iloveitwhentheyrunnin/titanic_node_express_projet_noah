d67c180a6d9be2ae75c8e541cae03b45b7b33b33
iloveitwhentheyrunnin <74145318+iloveitwhentheyrunnin@users.noreply.github.com>
2023-10-26 08:55:56 +0200

    login with mongo db user name & password + login controller

--- titanic_project/controllers/login.js
+++ titanic_project/controllers/login.js
@@ -1,14 +1,26 @@
 import UsersModel from "../models/User.js";
 
+export function LoginController(req, res) {
+  res.render('login.twig',{title: 'Login to the Titanic Tool'})
+}
+
 export async function PostLoginController(req, res) {
 
-  const docs = await UsersModel.find({ name : req.body.name });
-  console.log(docs);
+  const docs = await UsersModel.findOne({
+    name: req.body.name,
+  })
 
-  if(docs == null){
-    res.redirect('/login')
-  } else {
+  const { name, password } = req.body;
+
+  if (name === docs.name && password === docs.password) {
+    req.session.user = {
+      isLogged: true,
+    }
+
     res.redirect('/dashboard')
+
+  } else {
+    res.redirect('/login')
   }
 }
 
--- titanic_project/router.js
+++ titanic_project/router.js
@@ -1,23 +1,10 @@
 import express from 'express';
 import { Router } from 'express';
-import {PostLoginController } from './controllers/login.js';
+import {PostLoginController, LoginController } from './controllers/login.js';
 const router = Router()
 
-// login user
-router.route("/login").get(function(req,res){
-    res.render("login.twig",{title:'User Login'});
-})
-/*.post(function(req,res){
-    if(req.body.name == credential.name && req.body.password == credential.password){
-        req.session.user = req.body.name;
-        res.redirect('/dashboard');
-    }else{
-        res.end("Invalid Credentials")
-        res.redirect('/login');
-    }
-}) */
 
-
+router.get('/login', LoginController);
 // route for dashboard
 router.get('/dashboard', (req,res) => {
     if(req.session.user){
